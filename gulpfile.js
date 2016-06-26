b3e87cf789d6747b6accd3710c8febbd7900a5b4
Ben Christel <bchristel@pivotal.io>
2016-06-26 10:55:36 -0700

    Convert gulpfile to ES3
    - CircleCI has an older version of Node that doesn't know ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,41 @@
 'use strict'
 
-const gulp = require('gulp')
-const iife = require('gulp-iife')
-const concat = require('gulp-concat')
-const jasmine = require('gulp-jasmine')
-const browserify = require('browserify')
-const babel = require('gulp-babel')
-const source = require('vinyl-source-stream')
-const buffer = require('vinyl-buffer')
-const sourceMaps = require('gulp-sourcemaps')
-const esLint = require('gulp-eslint')
-const plumber = require('gulp-plumber')
+var gulp = require('gulp')
+var iife = require('gulp-iife')
+var concat = require('gulp-concat')
+var jasmine = require('gulp-jasmine')
+var browserify = require('browserify')
+var babel = require('gulp-babel')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var sourceMaps = require('gulp-sourcemaps')
+var esLint = require('gulp-eslint')
+var plumber = require('gulp-plumber')
 
-gulp.task('watch', () => {
-  return gulp.series(build, printDivider)(() => {
+gulp.task('watch', function () {
+  return gulp.series(build, printDivider)(function () {
     // beware!
     // Due to a bug (?) in one of gulp's dependencies,
     // `gulp.watch` will not notice created or deleted files
     // in paths beginning with './'
-    gulp.watch(['src/**/*.js', 'gulpfile.js'],
-      gulp.series(build, printDivider))
+    gulp.watch(['src/**/*.js', 'gulpfile.js'], gulp.series(build, printDivider))
 
-    return gulp.watch(['spec/**/*.js'],
-      gulp.series(check, printDivider))
+    return gulp.watch(['spec/**/*.js'], gulp.series(check, printDivider))
   })
 })
 
 function printDivider (done) {
-  const time = new Date().toTimeString().slice(0, 8)
-  const message = `==[finished at ${time}]`
+  var time = new Date().toTimeString().slice(0, 8)
+  var message = '==[finished at ' + time + ']'
 
   console.log(message + Array(80 - message.length).fill('=').join(''))
   done()
 }
 
-const check = gulp.series(test, lint)
+var check = gulp.series(test, lint)
 
 function test (done) {
-  const source = [
+  var source = [
     'src/shared/prelude.js',
     'src/*/lib/**/*.js',
     'spec/**/*.js'
@@ -49,7 +47,7 @@ function test (done) {
     .pipe(compileES2015())
     .pipe(jasmine())
     .on('jasmineDone', done)
-    .on('error', (err) => {
+    .on('error', function (err) {
       console.error(err.message)
       done()
     })
@@ -62,10 +60,10 @@ function lint (done) {
     .pipe(esLint.format('stylish', process.stdout))
 }
 
-const build = gulp.series(concatBrowser, distBrowser, distServer)
+var build = gulp.series(concatBrowser, distBrowser, distServer)
 
 function distBrowser (done) {
-  browserify('tmp/browser.js', {debug: true})
+  browserify('tmp/browser.js', { debug: true })
     .bundle()
     .pipe(source('browser.js'))
     .pipe(buffer())
@@ -74,13 +72,13 @@ function distBrowser (done) {
 }
 
 function concatBrowser (done) {
-  const source = [
+  var source = [
     'src/shared/prelude.js',
     'src/@(browser|shared)/lib/**/*.js',
     'src/browser/main.js'
   ]
 
-  return gulp.src(source, {base: 'src'})
+  return gulp.src(source, { base: 'src' })
     .pipe(plumber())
     .pipe(sourceMaps.init())
       .pipe(compileES2015())
@@ -92,13 +90,13 @@ function concatBrowser (done) {
 }
 
 function distServer (done) {
-  const source = [
+  var source = [
     'src/shared/prelude.js',
     'src/@(server/shared)/lib/**/*.js',
     'src/server/main.js'
   ]
 
-  return gulp.src(source, {base: 'src'})
+  return gulp.src(source, { base: 'src' })
     .pipe(plumber())
     .pipe(iife())
     .pipe(concat('server.js'))
@@ -120,5 +118,5 @@ gulp.task('concat-browser', concatBrowser)
 gulp.task('dist-server', distServer)
 
 function compileES2015 () {
-  return babel({presets: ['es2015']})
+  return babel({ presets: ['es2015'] })
 }
