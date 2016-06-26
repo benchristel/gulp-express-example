f950ee76ef9f140776da90c1e8addad8a12b523e
Ben Christel <bchristel@pivotal.io>
2016-06-25 20:08:36 -0700

    Upgrade to Gulp 4
    Gulp 4 makes many improvements, including:
    
    - makes it much simpler to run tasks in parallel or series
    - tasks are just functions now - so you can parameterize their inputs (via
      currying), make private "tasks", and invoke them manually.

--- gulpfile.js
+++ gulpfile.js
@@ -10,63 +10,67 @@ const source = require('vinyl-source-stream')
 const buffer = require('vinyl-buffer')
 const sourceMaps = require('gulp-sourcemaps')
 const esLint = require('gulp-eslint')
-const sequence = require('run-sequence')
+const plumber = require('gulp-plumber')
 
 gulp.task('watch', () => {
-  sequence('build', 'print-divider')
+  return gulp.series(build, printDivider)(() => {
+    // beware!
+    // Due to a bug (?) in one of gulp's dependencies,
+    // `gulp.watch` will not notice created or deleted files
+    // in paths beginning with './'
+    gulp.watch(['src/**/*.js', 'gulpfile.js'],
+      gulp.series(build, printDivider))
 
-  // beware!
-  // Due to a bug (?) in one of gulp's dependencies,
-  // `gulp.watch` will not notice created or deleted files
-  // in paths beginning with './'
-  gulp.watch(['src/**/*.js', 'gulpfile.js'], () =>
-    sequence('build', 'print-divider')
-  )
-
-  gulp.watch(['spec/**/*.js'], () =>
-    sequence('check', 'print-divider')
-  )
+    return gulp.watch(['spec/**/*.js'],
+      gulp.series(check, printDivider))
+  })
 })
 
-gulp.task('print-divider', () => {
+function printDivider (done) {
   const time = new Date().toTimeString().slice(0, 8)
   const message = `==[finished at ${time}]`
 
   console.log(message + Array(80 - message.length).fill('=').join(''))
-})
+  done()
+}
 
-gulp.task('build', ['check', 'dist-browser', 'dist-server'])
+const check = gulp.series(test, lint)
 
-gulp.task('check', ['test', 'lint'])
-
-gulp.task('test', () => {
+function test (done) {
   const source = [
     'src/shared/prelude.js',
     'src/*/lib/**/*.js',
     'spec/**/*.js'
   ]
 
-  return gulp.src(source)
+  gulp.src(source)
+    .pipe(plumber())
     .pipe(iife())
     .pipe(compileES2015())
     .pipe(jasmine())
-})
+    .on('jasmineDone', done)
+    .on('error', () => done())
+}
 
-gulp.task('lint', () => {
+function lint (done) {
   return gulp.src(['@(src|spec)/**/*.js', '*.js'])
+    .pipe(plumber())
     .pipe(esLint())
-    .pipe(esLint.format())
-})
+    .pipe(esLint.format('stylish', process.stdout))
+}
 
-gulp.task('dist-browser', ['concat-browser'], () => {
-  return browserify('tmp/browser.js', {debug: true})
+const build = gulp.series(concatBrowser, distBrowser, distServer)
+
+function distBrowser (done) {
+  browserify('tmp/browser.js', {debug: true})
     .bundle()
     .pipe(source('browser.js'))
     .pipe(buffer())
     .pipe(gulp.dest('dist/public/js'))
-})
+    .on('end', done)
+}
 
-gulp.task('concat-browser', () => {
+function concatBrowser (done) {
   const source = [
     'src/shared/prelude.js',
     'src/@(browser|shared)/lib/**/*.js',
@@ -74,15 +78,17 @@ gulp.task('concat-browser', () => {
   ]
 
   return gulp.src(source, {base: 'src'})
+    .pipe(plumber())
     .pipe(sourceMaps.init())
       .pipe(compileES2015())
       .pipe(iife())
       .pipe(concat('browser.js'))
     .pipe(sourceMaps.write())
     .pipe(gulp.dest('tmp/'))
-})
+    .on('end', done)
+}
 
-gulp.task('dist-server', () => {
+function distServer (done) {
   const source = [
     'src/shared/prelude.js',
     'src/@(server/shared)/lib/**/*.js',
@@ -90,11 +96,25 @@ gulp.task('dist-server', () => {
   ]
 
   return gulp.src(source, {base: 'src'})
+    .pipe(plumber())
     .pipe(iife())
     .pipe(concat('server.js'))
     .pipe(compileES2015())
     .pipe(gulp.dest('dist'))
-})
+    .on('end', done)
+}
+
+gulp.task('build', gulp.series(check, build))
+
+gulp.task('check', check)
+
+gulp.task('test', test)
+
+gulp.task('lint', lint)
+
+gulp.task('concat-browser', concatBrowser)
+
+gulp.task('dist-server', distServer)
 
 function compileES2015 () {
   return babel({presets: ['es2015']})
